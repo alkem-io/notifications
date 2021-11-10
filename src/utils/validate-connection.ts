b08a86a025d88c8b88ff07c267a855f230f01e09
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-11-10 19:20:36 +0200

    Fixed PR comments

--- src/utils/validate-connection.ts
+++ src/utils/validate-connection.ts
@@ -1,34 +1,41 @@
 import { AlkemioClient } from '@alkemio/client-lib';
+import {
+  DEFAULT_ENDPOINTS,
+  DEFAULT_SERVICE_ACCOUNT_CREDENTIALS,
+} from '@src/common/constants/defaults';
 import * as dotenv from 'dotenv';
 
 const main = async () => {
   dotenv.config();
 
-  const ctClient = new AlkemioClient({
+  const alkemioClient = new AlkemioClient({
     graphqlEndpoint:
-      process.env.ALKEMIO_SERVER_ENDPOINT ??
-      'http://localhost:3000/admin/graphql',
+      process.env.ALKEMIO_SERVER_ENDPOINT ?? DEFAULT_ENDPOINTS.alkemioServer,
     authInfo: {
       credentials: {
-        email: process.env.SERVICE_ACCOUNT_USERNAME ?? 'admin@alkem.io',
-        password: process.env.SERVICE_ACCOUNT_PASSWORD ?? 'obichamazis',
+        email:
+          process.env.SERVICE_ACCOUNT_USERNAME ??
+          DEFAULT_SERVICE_ACCOUNT_CREDENTIALS.email,
+        password:
+          process.env.SERVICE_ACCOUNT_PASSWORD ??
+          DEFAULT_SERVICE_ACCOUNT_CREDENTIALS.password,
       },
       kratosPublicApiEndpoint:
         process.env.KRATOS_API_PUBLIC_ENDPOINT ??
-        'http://localhost:3000/identity/ory/kratos/public',
+        DEFAULT_ENDPOINTS.kratosApiEndpoint,
     },
   });
 
-  console.log(`Client config: ${JSON.stringify(ctClient.config)}`);
-  await ctClient.enableAuthentication();
+  console.log(`Client config: ${JSON.stringify(alkemioClient.config)}`);
+  await alkemioClient.enableAuthentication();
 
-  console.log(`API Token: ${ctClient.apiToken}`);
+  console.log(`API Token: ${alkemioClient.apiToken}`);
 
-  const serverVersion = await ctClient.validateConnection();
+  const serverVersion = await alkemioClient.validateConnection();
   console.log(`Alkemio platform version: ${serverVersion}`);
 
   const hubID = 'Test';
-  const hubExists = await ctClient.hubExists(hubID);
+  const hubExists = await alkemioClient.hubExists(hubID);
   console.log(`Hub '${hubID}' exists: ${hubExists}`);
 };
 
