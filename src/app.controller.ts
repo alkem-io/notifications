505e4e6a97c9e23905b9158795c21abd2b197d72
Neil Smyth <neil@thesmyths.eu>
2021-12-01 07:43:39 +0100

    added vscode debug setup; added entry for user registration events; added sample payload for user registered; renamed ecoverseID to hubID in config; adjusted readme instructions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,11 +6,14 @@ import { ALKEMIO_CLIENT_ADAPTER, LogContext } from './common';
 import { IFeatureFlagProvider } from '@core/contracts';
 import { ApplicationCreatedEventPayload } from '@src/types/application.created.event.payload';
 import { ApplicationCreatedNotifier } from './services';
+import { UserRegistrationEventPayload } from './types';
+import { UserRegistrationNotifier } from './services/domain/user-registration-notifier/user.registration.notifier';
 
 @Controller()
 export class AppController {
   constructor(
     private applicationCreatedNotifier: ApplicationCreatedNotifier,
+    private userRegisteredNotifier: UserRegistrationNotifier,
     @Inject(WINSTON_MODULE_NEST_PROVIDER)
     private readonly logger: LoggerService,
     @Inject(ALKEMIO_CLIENT_ADAPTER)
@@ -35,6 +38,57 @@ export class AppController {
 
     // https://www.squaremobius.net/amqp.node/channel_api.html#channel_nack
     this.applicationCreatedNotifier
+      .sendNotifications(payload)
+      // .then(x => {
+      //   const nacked = x.filter(y => y.status === 'rejected');
+
+      //   if (nacked.length === 0) {
+      //     this.logger.verbose?.(`All ${x.length} messages successfully sent!`);
+      //     // if all is fine, acknowledge the given message. allUpTo (second, optional parameter) defaults to false,
+      //     // so only the message supplied is acknowledged.
+      //     channel.ack(originalMsg);
+      //   } else {
+      //     if (nacked.length === x.length) {
+      //       this.logger.verbose?.('All messages failed to be sent!');
+      //       // if all messages failed to be sent, we reject the message but we make sure the message is
+      //       // not discarded so we provide 'true' to requeue parameter
+      //       channel.reject(originalMsg, true);
+      //     } else {
+      //       this.logger.verbose?.(
+      //         `${nacked.length} messages out of total ${x.length} messages failed to be sent!`
+      //       );
+      //       // if at least one message is sent successfully, we acknowledge just this message but we make sure the message is
+      //       // dead-lettered / discarded, providing 'false' to the 3rd parameter, requeue
+      //       channel.nack(originalMsg, false, false);
+      //     }
+      //   }
+      // })
+      .catch(err => {
+        // if there is an unhandled bug in the flow, we reject the message but we make sure the message is
+        // not discarded so we provide 'true' to requeue parameter
+        channel.reject(originalMsg, true);
+        this.logger.error(err);
+      });
+  }
+
+  @EventPattern('userRegistration')
+  async sendUserRegisteredNotification(
+    // todo is auto validation possible
+    @Payload() payload: UserRegistrationEventPayload,
+    @Ctx() context: RmqContext
+  ) {
+    this.logger.verbose?.(JSON.stringify(payload), LogContext.NOTIFICATIONS);
+
+    const channel: Channel = context.getChannelRef();
+    const originalMsg = context.getMessage() as Message;
+
+    if (!(await this.featureFlagProvider.areNotificationsEnabled())) {
+      channel.ack(originalMsg);
+      return;
+    }
+
+    // https://www.squaremobius.net/amqp.node/channel_api.html#channel_nack
+    this.userRegisteredNotifier
       .sendNotifications(payload)
       // .then(x => {
       //   const nacked = x.filter(y => y.status === 'rejected');
