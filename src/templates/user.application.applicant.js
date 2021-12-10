c882f9117cb2ff935eabb8c2adb8b10544fcec73
Valentin Yanakiev <valentin.yanakiev@gmail.com>
2021-12-10 17:43:04 +0200

    Revert template changes

--- src/templates/user.application.applicant.js
+++ src/templates/user.application.applicant.js
@@ -15,21 +15,6 @@ module.exports = () => ({
           Please view the status of your application on your <a href="{{applicant.profile}}">profile</a>.<br><br>
           Sincerely yours,
           Team Alkemio
-        {% endblock %}
-
-        {% block footer %}
-        <td>
-        <b>Team Alkemio</b>
-        <br>
-        <br>
-        <a href="https://alkem.io">
-          <img src="https://alkem.io/uploads/logos/alkemio-logo.png" width="175"> </a>
-
-         We have received your application for <a href="{{community.url}}">{{community.name}}</a> [{{community.type}}]!<br><br>
-         Website: <a style="color:#068293; font-weight:600; text-decoration:underline;" href="https://alkem.io">https://alkem.io</a>
-
-
-        </td>
         {% endblock %}`,
     },
   },
