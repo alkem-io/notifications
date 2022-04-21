6f89936c33ddeeb1747dcc71c650b84e58251768
Svetoslav Petkov <svetoslav@alkem.io>
2022-04-21 10:59:20 +0300

    fixed community new member template

--- src/templates/community.new.member.admin.js
+++ src/templates/community.new.member.admin.js
@@ -3,13 +3,13 @@ const templates = require('./alkemio.template.blocks');
 /* eslint-disable quotes */
 module.exports = () => ({
   name: 'community-new-member-admin',
-  title: 'User {{member.displayName}} joined the {{community.name}} community',
+  title: 'User {{member.name}} joined the {{community.name}} community',
   version: 1,
   channels: {
     email: {
       from: '{{emailFrom}}',
       to: '{{recipient.email}}',
-      subject: 'User {{member.displayName}} joined the {{community.name}} community',
+      subject: 'User {{member.name}} joined the {{community.name}} community',
       html: `{% extends "src/templates/_layouts/email-transactional.html" %}
         {% block content %}
           Hi {{recipient.firstname}},<br><br>
