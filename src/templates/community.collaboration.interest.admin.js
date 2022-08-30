b19a9065ff270e7f3373d0925fc6883889f611f1
Neil Smyth <30729240+techsmyth@users.noreply.github.com>
2022-08-30 11:04:38 +0200

    expanded the inforation sent with collaboration interest notification (#138)

--- src/templates/community.collaboration.interest.admin.js
+++ src/templates/community.collaboration.interest.admin.js
@@ -9,13 +9,18 @@ module.exports = () => ({
     email: {
       from: '{{emailFrom}}',
       to: '{{recipient.email}}',
-      subject: 'New user is interested to collaborate on opportunity',
+      subject:
+        '{{user.name}} is interested to collaborate on opportunity: {{opportunity.name}}',
       html: `{% extends "src/templates/_layouts/email-transactional.html" %}
         {% block content %}
           Hi {{recipient.firstname}},<br><br>
 
-          The user {{user.name}} is interested to participate in {{opportunity.name}} opportunity.
-          <br/><br/>
+          The user '{{user.name}}' is interested to participate in '{{opportunity.name}}' opportunity:
+          <br/>
+          - role: {{relation.role}}<br/>
+          - description: {{relation.description}}<br/>
+          <br/>
+          <br/>
           Sincerely yours,
         {% endblock %}
 
