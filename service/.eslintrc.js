c576b84f4aaae4d46922871d85699f7361945c1d
Aleksandar <aleksandarstojanovic.coa@gmail.com>
2022-11-30 10:49:15 +0100

    Update config files

--- service/.eslintrc.js
+++ service/.eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './service/tsconfig.json',
     sourceType: 'module',
   },
-  //tsconfigRootDir: './',
+  tsconfigRootDir: './',
   plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
