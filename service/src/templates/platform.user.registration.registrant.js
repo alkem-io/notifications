2837a3cd5ace863f99068b2cb0672e3edcfe0e89
Simone <38861315+SimoneZaza@users.noreply.github.com>
2024-02-26 13:47:32 +0100

    Updated new platform email - Please check first :) Update platform.user.regist… (#288)
    * Updated new platform email - Check first! Update platform.user.registration.registrant.js
    
    * Bump service version.
    
    * Update user registration template.
    
    ---------
    
    Co-authored-by: Aleksandar <aleksandarstojanovic.coa@gmail.com>

--- service/src/templates/platform.user.registration.registrant.js
+++ service/src/templates/platform.user.registration.registrant.js
@@ -11,19 +11,19 @@ module.exports = () => ({
       to: '{{recipient.email}}',
       subject: 'Alkemio - Registration successful!',
       html: `{% extends "src/templates/_layouts/email-transactional.html" %}
-        {% block content %}Hi {{registrant.firstName}}, welcome to the Alkemio platform! <br>
+        {% block content %}
+    <p>Dear {{registrant.firstName}},</p>
+    <p>Welcome aboard the Alkemio platform! Your account creation was a success — congratulations! 🎉 Perhaps you have already explored the platform, but let’s ensure you are fully equipped to dive in with the links below.</p><br>
+        <p>🌐 <a style="color:#000000; text-decoration: none;" href="https://alkem.io/home"><b>Find Spaces</b>: At Alkemio, users collaborate within Spaces. If you are looking for a particular Space, use the search functionality on your Dashboard</a>.</p>
+        <p>🚀 <a style="color:#000000; text-decoration: none;" href="https://alkem.io/welcome-space"><b>Explore the possibilities</b>: Take a peek at our Welcome Space — it’s an open Space for you to explore the platform's structure and functionality.</a></p>
+        <p>📸 <a style="color:#000000; text-decoration: none;" href="{{registrant.profile}}"><b>Personalize Your Profile</b>: Add a friendly photo to your profile so fellow users can put a face to your name.</a></p>
+        <p>💌 <a style="color:#000000; text-decoration: none;" href="mailto:community@alkem.io"><b>Need Assistance?</b>: Whether you’re keen on starting your own Space or require support, our Community team is here for you. Reach out anytime at <b>community@alkem.io</b></a>.</p>
+        <p><a style="color:#000000; text-decoration: none;" href="https://welcome.alkem.io">❔ <b>Learn More</b>: For additional details about Alkemio, check out our website</a>.</p>
         <br>
-          Your account has been successfully created. Please spend some time to further populate your <a style="color:#065F6B; text-decoration: none;" href="{{registrant.profile}}" >profile</a> so that other users and organizations can find you.
-          <br><br>
-          <a class="action-button" href="{{registrant.profile}}">YOUR PROFILE</a><br><br>
-          On the <a style="color:#065F6B; text-decoration: none;" href="https://alkem.io/home">Homepage</a> and <a style="color:#065F6B; text-decoration: none;" href="https://alkem.io/challenges">Challenges page</a> you'll find all available Spaces and Challenges for you to join. 
-          <br>
-          Looking forward to seeing your interactions and contributions!<br><br>
-
-          Sincerely yours,<br><br>
-
-          The Alkemio team
-          <br><br>
+    <p>We are looking forward to seeing your interactions and contributions!</p>
+    <p>Warm regards,</p>
+    <p>The Alkemio Team 🌟</p>
+    <br><br>
         {% endblock %}
         ${templates.footerBlock}`,
     },
