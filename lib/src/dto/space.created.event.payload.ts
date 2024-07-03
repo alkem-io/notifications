9ec598d57c688b57342f0a1f820d7d103c3cc879
Emilia Pavlova <emilia.pavlova@gmail.com>
2024-07-03 13:29:02 +0300

    update type

--- lib/src/dto/space.created.event.payload.ts
+++ lib/src/dto/space.created.event.payload.ts
@@ -2,4 +2,8 @@ import { SpaceBaseEventPayload } from "./space.base.event.payload";
 
 export interface SpaceCreatedEventPayload extends SpaceBaseEventPayload {
   created: number;
+  sender: {
+    name: string;
+    url: string;
+  }
 }
