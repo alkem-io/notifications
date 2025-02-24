671c824dc7051cd2dd9ab28ce32acf86781fd24c
Svetoslav <svetoslav@alkem.io>
2025-02-24 16:18:19 +0200

    enums updated according to latest graphql schema

--- lib/src/dto/in-app/in.app.notification.contributor.mentioned.payload.ts
+++ lib/src/dto/in-app/in.app.notification.contributor.mentioned.payload.ts
@@ -1,11 +1,11 @@
-import { CommunityContributorType } from "@alkemio/client-lib";
+import { RoleSetContributorType } from "@alkemio/client-lib";
 import { InAppNotificationPayloadBase } from "./in.app.notification.payload.base";
 import { NotificationEventType } from '../../notification.event.type';
 
 export interface InAppNotificationContributorMentionedPayload extends InAppNotificationPayloadBase {
   type: NotificationEventType.COMMUNICATION_USER_MENTION;
   comment: string; // probably will be removed; can be too large; can be replaced with roomID, commentID
-  contributorType: CommunityContributorType
+  contributorType: RoleSetContributorType
   commentOrigin: {
     displayName: string;
     url: string;
--- lib/src/dto/platform.user.invited.to.role.event.payload.ts
+++ lib/src/dto/platform.user.invited.to.role.event.payload.ts
@@ -1,11 +1,11 @@
-import { PlatformRole } from "@alkemio/client-lib";
+import { RoleName } from "@alkemio/client-lib";
 import { BaseEventPayload } from "./base.event.payload";
 import { ContributorPayload } from "./contributor.payload";
 
 export interface PlatformUserInvitedToRoleEventPayload
   extends BaseEventPayload {
     user: ContributorPayload;
-    role: PlatformRole;
+    role: RoleName;
     invitees: {
       email: string;
     }[];
