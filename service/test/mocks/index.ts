b9faf716462afaaa8a8ea6d37b1721e2ceb43686
Neil Smyth <neil@thesmyths.eu>
2025-08-22 08:36:16 +0200

    partial fix of tests

--- service/test/mocks/index.ts
+++ service/test/mocks/index.ts
@@ -1,5 +1,3 @@
 export * from './config.service.mock';
 export * from './winston.provider.mock';
-export * from './alkemio.client.adapter.provider.mock';
 export * from './notifme.provider.mock';
-export * from './alkemio.client.provider.mock';
