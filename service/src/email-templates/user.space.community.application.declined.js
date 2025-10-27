cce38701054e5a75093965001f213bf7f33aab45
bobbykolev <bobbykolev@abv.bg>
2025-10-27 14:32:08 +0200

    application insted of invitation for application declined email wording

--- service/src/email-templates/user.space.community.application.declined.js
+++ service/src/email-templates/user.space.community.application.declined.js
@@ -3,15 +3,15 @@ const templates = require('./alkemio.template.blocks');
 /* eslint-disable quotes */
 module.exports = () => ({
   name: 'user.space.community.application.declined',
-  title: '[{{space.displayName}}] {{decliner.name}} declined your invitation',
+  title: '[{{space.displayName}}] {{decliner.name}} declined your application',
   version: 1,
   channels: {
     email: {
       to: '{{recipient.email}}',
-      subject: '{{decliner.name}} declined your invitation for {{space.displayName}}',
+      subject: '{{decliner.name}} declined your application for {{space.displayName}}',
       html: `{% extends "src/email-templates/_layouts/email-transactional.html" %}
         {% block content %}Hi {{recipient.firstName}},<br>
-          <a href="{{decliner.profile}}">{{decliner.firstName}}</a> declined your invitation for {{space.displayName}}.
+          <a href="{{decliner.profile}}">{{decliner.firstName}}</a> declined your application for {{space.displayName}}.
           <br><br>
           <a class="action-button" href="{{spaceURL}}">Have a look</a><br><br>
         {% endblock %}
