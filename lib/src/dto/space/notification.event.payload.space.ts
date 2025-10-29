f4c8e5742733ca3259670a5b4b6cb4e2421e1cad
bobbykolev <bobbykolev@abv.bg>
2025-10-29 11:25:25 +0200

    lib space type update

--- lib/src/dto/space/notification.event.payload.space.ts
+++ lib/src/dto/space/notification.event.payload.space.ts
@@ -4,4 +4,10 @@ import { SpacePayload } from "./space.payload";
 export interface NotificationEventPayloadSpace
   extends BaseEventPayload {
   space: SpacePayload;
+  calendarEvent?: {
+    id: string;
+    title: string;
+    type: string;
+    createdBy: string;
+  };
 }
